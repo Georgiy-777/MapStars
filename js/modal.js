49344fb593c09636fd70ec1e7410c3bdd2bfa5fa
Georgiy-777 <demirov709079@gmail.com>
2022-12-10 09:04:34 +0200

    stage 2

---
+++ js/modal.js
@@ -0,0 +1,73 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    var modalButtons = document.querySelectorAll('.get-btn'),
+         overlay      = document.querySelector('.overlay'),
+        closeButtons = document.querySelectorAll('.modal__close-btn'),
+        modalNavbar = document.querySelector('.modal__navbar-link');
+ 
+ 
+    /* Перебираем массив кнопок */
+    modalButtons.forEach(function(item){
+ 
+       /* Назначаем каждой кнопке обработчик клика */
+       item.addEventListener('click', function(e) {
+ 
+          /* Предотвращаем стандартное действие элемента. Так как кнопку разные
+             люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
+             Нужно подстраховаться. */
+          e.preventDefault();
+ 
+          /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
+             и будем искать модальное окно с таким же атрибутом. */
+          var modalId = this.getAttribute('data-modal'),
+              modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
+ 
+ 
+          /* После того как нашли нужное модальное окно, добавим классы
+             подложке и окну чтобы показать их. */
+          modalElem.classList.add('active');
+          overlay.classList.add('active');
+       }); // end click
+ 
+    }); // end foreach
+ 
+ 
+    closeButtons.forEach(function(item){
+ 
+       item.addEventListener('click', function(e) {
+          var parentModal = this.closest('.modal');
+ 
+          parentModal.classList.remove('active');
+          overlay.classList.remove('active');
+       });
+ 
+    }); // end foreach
+ 
+    modalNavbar.addEventListener('click', function(){
+      // var parent = this.closest('.modal');
+ 
+      document.querySelector('.modal.active').classList.remove('active');
+      document.querySelector('.overlay.active').classList.remove('active');
+
+    })
+ 
+     document.body.addEventListener('keyup', function (e) {
+         var key = e.keyCode;
+ 
+         if (key == 27) {
+ 
+             document.querySelector('.modal.active').classList.remove('active');
+             document.querySelector('.overlay').classList.remove('active');
+         };
+     }, false);
+ 
+ 
+     overlay.addEventListener('click', function() {
+         document.querySelector('.modal.active').classList.remove('active');
+         this.classList.remove('active');
+     });
+ 
+ 
+ 
+ 
+ }); // end ready
